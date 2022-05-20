537e56e377d03e5ff340132a389136d3545b510a
Domeniqque Dylluar <domeniqque@hotmail.com>
2022-05-20 05:20:02 -0400

    cleanup

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders movie search link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/movie search/i);
   expect(linkElement).toBeInTheDocument();
 });
